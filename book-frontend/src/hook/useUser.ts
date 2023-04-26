946514dc0095575eb74994f0d1d1caaf788bbe8b
com.xin <kleine4augen@gmail.com>
2023-04-26 20:45:56 +0200

    fix usermodel

--- book-frontend/src/hook/useUser.ts
+++ book-frontend/src/hook/useUser.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
-import {User} from "../model/UserModel";
+import {User,UserModel} from "../model/UserModel";
 
 export default function useUser() {
     const [user, setUser] = useState<User>();
