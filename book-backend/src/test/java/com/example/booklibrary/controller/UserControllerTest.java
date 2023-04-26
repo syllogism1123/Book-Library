09063253433f65d72c942983886aed5d64a4d09b
com.xin <kleine4augen@gmail.com>
2023-04-26 16:46:22 +0200

    modify book integration test

--- book-backend/src/test/java/com/example/booklibrary/controller/UserControllerTest.java
+++ book-backend/src/test/java/com/example/booklibrary/controller/UserControllerTest.java
@@ -1,26 +1,18 @@
 package com.example.booklibrary.controller;
 
-import com.example.booklibrary.model.MongoUser;
 import com.example.booklibrary.model.MongoUserDTO;
-import com.example.booklibrary.repository.MongoUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Optional;
-
-import static org.hamcrest.CoreMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -35,10 +27,6 @@ class UserControllerTest {
     private MockMvc mvc;
     @Autowired
     private JacksonTester<MongoUserDTO> json;
-
-    @MockBean
-    private MongoUserRepository mongoUserRepository;
-
     @Test
     @DirtiesContext
     @WithMockUser
